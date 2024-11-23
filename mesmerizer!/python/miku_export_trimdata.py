c5a8f58def08bc8bfc82563e4f1eee8c7b3fc2d5
ramune314159265 <ramune314159265@gmail.com>
2024-11-23 21:05:03 +0900

    背景とか用

--- mesmerizer!/python/miku_export_trimdata.py
+++ mesmerizer!/python/miku_export_trimdata.py
@@ -1,21 +1,21 @@
 import glob
 import json
 
-import trimdata_functions
+import chara_trimdata_functions
 
 functions = [
-	{"until":96, "fn": trimdata_functions.blank},
-	{"until":330, "fn": trimdata_functions.fixed_position(232, 575)},
-	{"until":850, "fn": trimdata_functions.flame_left},
-	{"until":1039, "fn": trimdata_functions.flame_right},
-	{"until":1069, "fn": trimdata_functions.flame_left},
-	{"until":1900, "fn": trimdata_functions.fixed_position(122, 575)},
-	{"until":2147, "fn": trimdata_functions.flame_left},
-	{"until":2333, "fn": trimdata_functions.blank},
-	{"until":2889, "fn": trimdata_functions.fixed_position(232, 575)},
-	{"until":2909, "fn": trimdata_functions.flame_left},
-	{"until":3451, "fn": trimdata_functions.fixed_position(122, 575)},
-	{"until":3704, "fn": trimdata_functions.fixed_position(232, 575)},
+	{"until":96, "fn": chara_trimdata_functions.blank},
+	{"until":330, "fn": chara_trimdata_functions.fixed_position(232, 575)},
+	{"until":850, "fn": chara_trimdata_functions.flame_left},
+	{"until":1039, "fn": chara_trimdata_functions.flame_right},
+	{"until":1069, "fn": chara_trimdata_functions.flame_left},
+	{"until":1900, "fn": chara_trimdata_functions.fixed_position(122, 575)},
+	{"until":2147, "fn": chara_trimdata_functions.flame_left},
+	{"until":2333, "fn": chara_trimdata_functions.blank},
+	{"until":2889, "fn": chara_trimdata_functions.fixed_position(232, 575)},
+	{"until":2909, "fn": chara_trimdata_functions.flame_left},
+	{"until":3451, "fn": chara_trimdata_functions.fixed_position(122, 575)},
+	{"until":3704, "fn": chara_trimdata_functions.fixed_position(232, 575)},
 ]
 
 images = glob.glob("./all_flames/*")
